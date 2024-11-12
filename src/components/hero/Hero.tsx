4f0026704072937605bb326a5fd9d3afa2a960ad
Doston1002 <polatovdoston1002@gmail.com>
2024-11-12 16:18:48 +0500

    hero com

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,9 +1,9 @@
-import { Box } from '@mui/material'
+import { Avatar, Box, Typography } from '@mui/material'
 import 'react-multi-carousel/lib/styles.css';
 import Carousel from 'react-multi-carousel';
 import React from 'react'
 import Image from 'next/image';
-
+import {format} from 'date-fns'
 const Hero = () => {
   return (
     <Box width={'100%'} height={'70vh'} sx={{backgroundColor:'red'}}>
@@ -19,9 +19,32 @@ const Hero = () => {
                     <Box sx={{position:'relative', width:'100%', height:'70vh'}}>
 
                     <Image src={item.image} alt={item.title} fill style={{objectFit:'cover'}}/>
-                   <Box sx={{position:'absolute', top:0, left:0, right:0, bottom:0, width:'100%', height:'100%', backgroundColor: 'rgba(0,0,0,.6)'}}>
+                    <Box
+								sx={{
+									position: 'absolute',
+									top: 0,
+									left: 0,
+									right: 0,
+									bottom: 0,
+									width: '100%',
+									height: '100%',
+									backgroundColor: 'rgba(0, 0, 0, .6)',
+								}}
+							/>
+                            <Box position={'relative'} width={{xs:'100%', sm:'70%'}} color={'white'} sx={{ fontSize: { xs: '30px', md: '50px' }, top:'50%', transform:'translateY(-50%)', paddingLeft:{xs:'10px', sm:'50px'}}}>
 
+                   <Typography variant='h2'>{item.title}</Typography>
+                   <Typography  variant='h5'>{item.exerpt}</Typography>
+                   <Box sx={{display:'flex', gap:'10px', marginTop:'20px'}}>
+                        <Avatar alt={item.author.name} src={item.author.image}/>
+                   <Box>
+                        <Typography>{item.author.name}</Typography>
+                            <Box>
+                            {format(new Date(), 'dd MMM, yyyy')}&#x2022; 10 minut read
+                            </Box>
                    </Box>
+                   </Box>
+                            </Box>
                     </Box>
                 </Box>
             ))}
