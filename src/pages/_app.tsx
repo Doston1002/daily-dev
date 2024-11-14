199e9ee6ea06bfd676900c7c24b1bddb500bea28
Doston1002 <polatovdoston1002@gmail.com>
2024-11-14 18:38:38 +0500

    songisi

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,4 @@
-
 import 'src/styles/globals.css';
-import 'nprogress/nprogress.css';
-
 import type { AppProps } from 'next/app';
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import createEmotionCache from 'src/helpers/create-emotion-cashe';
@@ -9,9 +6,6 @@ import Head from 'next/head';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from 'src/helpers/theme';
 import { CssBaseline } from '@mui/material';
-import NProgress from 'nprogress';
-import { useEffect } from 'react';
-import Router from 'next/router';
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -21,21 +15,6 @@ export interface MyAppProps extends AppProps {
 
 function MyApp(props: MyAppProps) {
 	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-
-	useEffect(() => {
-		const handleRouteStart = () => NProgress.start();
-		const handleRouteDone = () => NProgress.done();
-
-		Router.events.on('routeChangeStart', handleRouteStart);
-		Router.events.on('routeChangeComplete', handleRouteDone);
-		Router.events.on('routeChangeError', handleRouteDone);
-
-		return () => {
-			Router.events.off('routeChangeStart', handleRouteStart);
-			Router.events.off('routeChangeComplete', handleRouteDone);
-			Router.events.off('routeChangeError', handleRouteDone);
-		};
-	}, []);
 
 	return (
 		<CacheProvider value={emotionCache}>
